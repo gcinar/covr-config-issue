a902325d1fcfeab8334e422f747dddc7c50b7e46
Goktug T. Cinar <goktugcinar@gmail.com>
2019-11-05 18:12:57 -0800

    Update test-case-2.R

--- tests/testthat/test-case-2.R
+++ tests/testthat/test-case-2.R
@@ -1,5 +1,5 @@
 
-expected <- config::get("some_config")
+expected <- config::get("some_config", file = system.file("config.yml", package = "covrconfig"))
 test_that("Issue type 2", {
   msg <- hello()
   expect_equal(msg, expected)
